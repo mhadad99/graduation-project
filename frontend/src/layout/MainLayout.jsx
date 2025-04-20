abfc32f575ef09fbcb4c34c9286912acfe7bc083
Mohamad Hosny <mohamadhosny4@example.com>
2025-04-20 15:56:05 +0200

    modify footer react and css

--- frontend/src/layout/MainLayout.jsx
+++ frontend/src/layout/MainLayout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Header } from '../components/Header'
 import { HomeHero } from '../components/HomeHero'
 import { HomeCategory } from '../components/HomeCategory'
-import { Footer } from '../components/Footer'
+import Footer from '../components/Footer'
 
 export function MainLayout() {
   return (
