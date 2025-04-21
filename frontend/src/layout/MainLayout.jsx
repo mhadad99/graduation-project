fb95d7a277deb6ad3908629c59ab657a0cdc85a1
Muhammad Hassan Haddad <mhadad315@gmail.com>
2025-04-21 21:13:54 +0200

    service details as static v1

--- frontend/src/layout/MainLayout.jsx
+++ frontend/src/layout/MainLayout.jsx
@@ -7,6 +7,7 @@ import {
   Routes,
 } from "react-router-dom";
 import {SharedLayout } from "../sharedLayout/SharedLayout";
+import { ServiceDetailsPage } from "../pages/ServiceDetailsPage";
 
 
 export function MainLayout() {
@@ -15,6 +16,7 @@ export function MainLayout() {
       <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<Home />} />
+          <Route path="/servicedetails" element={<ServiceDetailsPage />} />
           <Route path="*" element={<NotFound />} /> 
         </Route>
       </Routes>
