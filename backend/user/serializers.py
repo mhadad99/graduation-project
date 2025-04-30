ff2aff8d48c0cfb3a221b4a48b02c7e46f44c630
Mohamed akram <m.353akram@gmail.com>
2025-04-30 14:14:34 +0300

    register takes data for user only

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -12,8 +12,6 @@ class UserCreateSerializer(serializers.ModelSerializer):
     user_type = serializers.ChoiceField(
         choices=CustomUser.USER_TYPES[1:]
     )  # exclude 'none'
-    freelancer_data = FreelancerCreateSerializer(required=False)
-    client_data = ClientCreateSerializer(required=False)
 
     class Meta:
         model = CustomUser
@@ -28,38 +26,14 @@ class UserCreateSerializer(serializers.ModelSerializer):
             "phone",
             "birth_date",
             "user_type",
-            "freelancer_data",
-            "client_data",
         ]
         extra_kwargs = {
             "password": {"write_only": True},
-            "freelancer_data": {"write_only": True},
-            "client_data": {"write_only": True},
         }
 
     def create(self, validated_data):
-        user_type = validated_data.pop("user_type")
-        freelancer_data = validated_data.pop("freelancer_data", None)
-        client_data = validated_data.pop("client_data", None)
-
         validated_data["password"] = make_password(validated_data["password"])
-        user = CustomUser.objects.create(**validated_data, user_type=user_type)
-
-        if user_type == "freelancer":
-            if not freelancer_data:
-                raise serializers.ValidationError(
-                    {"freelancer_data": "Required for freelancer."}
-                )
-            Freelancer.objects.create(uid=user, **freelancer_data)
-
-        elif user_type == "client":
-            if not client_data:
-                raise serializers.ValidationError(
-                    {"client_data": "Required for client."}
-                )
-            Client.objects.create(uid=user, **client_data)
-
-        return user
+        return CustomUser.objects.create(**validated_data)
 
 
 class UserOutSerializer(serializers.ModelSerializer):
